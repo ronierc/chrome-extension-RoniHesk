a5552094c8f22911dd46266becbe5990dadc68ab
Ronier <ronier.cavalari@outlook.com>
2026-02-27 18:08:15 -0300

    Ajuste Layout

--- assets/js/fichas.js
+++ assets/js/fichas.js
@@ -1,21 +1,3 @@
-// Troca icones das fichas
-const troca = {
-  'img/icon_like.jpg': 'https://img.icons8.com/?size=20&id=ZAASfo9L21mn&format=png',
-  'img/flag-s.png': 'https://img.icons8.com/?size=20&id=70yRC8npwT3d&format=png',
-  'img/play-ft.png': 'https://img.icons8.com/?size=20&id=KQ5qZwOlaNdR&format=png',
-  'img/info.png': 'https://img.icons8.com/?size=20&id=VQOfeAx5KWTK&format=png',
-  '../img/print.png': 'https://img.icons8.com/?size=20&id=n9P6681cdRgv&format=png',
-  '../img/editar-ficha.png': 'https://img.icons8.com/?size=20&id=poyizCBxzCRY&format=png',
-};
-
-document
-  .querySelectorAll('img[src], input[type="image"][src]')
-  .forEach(el => {
-    const atual = el.getAttribute('src');
-    const novo = troca[atual];
-    if (novo) el.setAttribute('src', novo);
-  });
-
 // Alterar botão de criar ficha
 const imgCadFicha = document.querySelector('img[src="../img/nova-ficha-tecnica.png"]').parentElement
 
---
+++ assets/js/imgs.js
@@ -0,0 +1,22 @@
+// Troca icones das fichas
+const troca = {
+  'img/icon_like.jpg': 'https://img.icons8.com/?size=20&id=ZAASfo9L21mn&format=png',
+  'img/flag-s.png': 'https://img.icons8.com/?size=20&id=70yRC8npwT3d&format=png',
+  'img/play-ft.png': 'https://img.icons8.com/?size=20&id=KQ5qZwOlaNdR&format=png',
+  'img/info.png': 'https://img.icons8.com/?size=20&id=VQOfeAx5KWTK&format=png',
+  '../img/print.png': 'https://img.icons8.com/?size=20&id=n9P6681cdRgv&format=png',
+  '../img/editar-ficha.png': 'https://img.icons8.com/?size=20&id=poyizCBxzCRY&format=png',
+  '../img/tag_off.png': 'https://img.icons8.com/?size=20&id=XMbCezCL6pm3&format=png',
+  '../img/change.png': 'https://img.icons8.com/?size=20&id=m8gRP2AQ4AOX&format=png',
+  '../img/E.png': 'https://img.icons8.com/?size=20&id=dKMGP5XqWxob&format=png',
+  '../img/D.png': 'https://img.icons8.com/?size=20&id=IchwUEgoxNcw&format=png',
+};
+
+//Seleciona as img para trocar
+document
+  .querySelectorAll('img[src], input[type="image"][src]')
+  .forEach(el => {
+    const atual = el.getAttribute('src');
+    const novo = troca[atual];
+    if (novo) el.setAttribute('src', novo);
+  });
